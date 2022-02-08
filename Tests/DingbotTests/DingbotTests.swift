50d1b10b442276b4aeb2902c9c1d4e036c066e56
iWw <i.1214@yahoo.com>
2022-02-08 23:55:59 +0800

    add tests

--- Tests/DingbotTests/DingbotTests.swift
+++ Tests/DingbotTests/DingbotTests.swift
@@ -6,5 +6,17 @@ final class DingbotTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        
+        Dingbot.shared.configure = Dingbot.Configure(accessToken: "YOUR_ACCESS_TOKEN", signKey: "YOUR_SIGN_KEY")
+        
+        // Send text message
+        try await Dingbot.shared.send(Dingbot.TextMessage("test"))
+        // Send link message
+        try await Dingbot.shared.send(Dingbot.LinkMessage(title: "标题", text: "test, 内容", messageUrl: "https://iiiam.in", picUrl: nil))
+        // Send markdown message
+        try await Dingbot.shared.send(Dingbot.MarkdownMessage(title: "markdown title", content: "#### test markdown content\n\n> build 1214", at: nil))
+        
+        // Send to specific dingbot with accessToken (and signKey `nullable`)
+        try await Dingbot.shared.send(Dingbot.TextMessage("Only test text message"), configure: Dingbot.Configure(accessToken: "", signKey: ""))
     }
 }
